d4021212460e74fa733b25105a1c04a439b7500f
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 08:53:54 +0100

    Commit

---
+++ Program koji ispisuje tri najveća broja iz unetog niza/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Unesite niz brojeva odvojenih razmakom:");
+        int[] niz = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        if (niz.Length < 3)
+        {
+            Console.WriteLine("Niz mora sadržavati najmanje 3 broja.");
+        }
+        else
+        {
+            var najveci = niz.OrderByDescending(x => x).Take(3); // Sortira niz opadajuće i uzima prva 3 broja
+            Console.WriteLine("Tri najveća broja su: " + string.Join(" ", najveci));
+        }
+    }
+}
